b0f68d132d5e020bd24cf82a46cb207f941a1c87
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 16:52:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,4 +10,12 @@ def map_to_negativize(source_array)
   new_array
 end
     
-  
+def map_to_no_change(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.size do
+    new_array[i] = source_array[i]
+    i += 1
+  end
+  new_array
+end
