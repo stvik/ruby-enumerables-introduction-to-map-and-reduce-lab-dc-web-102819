4539f5c8661b05d6d0d7e3546bbf8611ffeebb45
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 16:54:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,3 +29,13 @@ def map_to_double(source_array)
   end
   new_array
 end
+
+def map_to_square(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.size do
+    new_array[i] = source_array[i]**2
+    i += 1
+  end
+  new_array
+end
