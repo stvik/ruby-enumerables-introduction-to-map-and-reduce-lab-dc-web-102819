290d2446b610645d2bcaee680fa728eec2224350
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 16:57:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,3 +39,17 @@ def map_to_square(source_array)
   end
   new_array
 end
+
+def reduce_to_total(source_array, starting_point)
+  total = 0
+  i = starting_point
+  while i < source_array.size do
+    total = total + source_array[i]
+    i +=1
+  end
+  total
+end
+    
+  
+  
+  
