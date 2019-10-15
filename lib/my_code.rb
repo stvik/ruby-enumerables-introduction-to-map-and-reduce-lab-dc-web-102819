f7fef909dd86695082dbdf02d7a8032c72390225
Sara Tarnvik <stvik2@gmail.com>
2019-10-15 17:06:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,7 +53,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.size do
-    if source_array[i] == true
+    if source_array[i] != false
       i += 1
     else 
       return false
